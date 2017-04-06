a434f65fd504f2f01a55b0557d7ab506546cba38
Noah Luck Easterly <noah.easterly@gmail.com>
2017-04-06 13:26:26 -0400

    Track tmux-vim.sh for It's All Text

---
+++ Applications/tmux-vim.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+open /Applications/Utilities/Terminal.app
+
+if tmux select-window -t ':<tmux-vim>' >/dev/null 2>&1 ; then
+  tmux select-pane -t ':<tmux-vim>.0' >/dev/null 2>&1
+  tmux send-keys -t ':<tmux-vim>' "C-[" ":tabe ${(q)*}" Enter
+else
+  tmux new-window -n '<tmux-vim>' "vim ${(q)*}"
+  tmux send-keys -t ':<tmux-vim>' "C-[" ":cd %:h" Enter
+  tmux set-option -t ':<tmux-vim>' allow-rename-off >/dev/null 2>&1
+fi
+
+true
