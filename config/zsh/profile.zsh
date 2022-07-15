f2d7e8acd9983ffa1a2cb23990c928dbd943f212
Noah Luck Easterly <noah.easterly@gmail.com>
2022-07-15 09:40:59 -0400

    Create profile.zsh

---
+++ config/zsh/profile.zsh
@@ -0,0 +1,19 @@
+#
+# $ZDOTDIR/.zprofile
+#
+# Sourced by zsh at startup when the shell is a login shell.
+
+# ------------------------------------------------------------------------------
+#                            Environment variables
+# ------------------------------------------------------------------------------
+
+# Change PRINTER to printer name near you
+
+
+# ------------------------------------------------------------------------------
+#                                Miscellaneous
+
+# ------------------------------------------------------------------------------
+
+# Set umask if you want to override the default value of 077.
+#umask 027
